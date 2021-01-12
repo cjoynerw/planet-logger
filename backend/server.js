89379cd1d42a6853c779a8bacfe994e0f414b3bc
cjoynerw <cjoynerw@gmail.com>
2021-01-12 16:25:41 -0600

    created create POST route

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,32 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 const fruits = require("./models/fruits.js");
 
+// Middleware
+app.use((req, res, next) => {
+  console.log("I run for all routes");
+  next();
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Routes
 app.get("/fruits/new", (req, res) => {
   res.render("new.ejs");
+});
+
+app.post("/fruits", (req, res) => {
+  if (req.body.readyToEat === "on") {
+    //if checked, req.body.readyToEat is set to 'on'
+    req.body.readyToEat = true;
+  } else {
+    //if not checked, req.body.readyToEat is undefined
+    req.body.readyToEat = false;
+  }
+  fruits.push(req.body);
+  res.redirect("/fruits"); //send the user back to /fruits
 });
 
 app.get("/fruits/:index", function (req, res) {
