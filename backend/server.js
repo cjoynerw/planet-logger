1e33bcb797d1e28938f22f28cb7987b0a4bc878a
cjoynerw <cjoynerw@gmail.com>
2021-01-12 16:00:28 -0600

    created views and models folders with fruit example that will be changed to planets

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
 
-app.get("/somedata", (req, res) => {
-  res.send("here is your info");
+const fruits = require("./models/fruits.js");
+
+app.get("/fruits/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.get("/fruits/:index", function (req, res) {
+  res.render("show.ejs", {
+    //second param must be an object
+    fruit: fruits[req.params.index], //there will be a variable available inside the ejs file called fruit, its value is fruits[req.params.index]
+  });
 });
 
 app.listen(3000, () => {
